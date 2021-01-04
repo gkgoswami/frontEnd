c5315d200d8b24fa2fbd2eec4c5531790d61a556
GAUTAM <48681982+gkgoswami@users.noreply.github.com>
2021-01-04 18:23:01 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CreateLink from './CreateLink';
+import LinkList from './LinkList';
+
+class App extends Component {
+  render() {
+//    return <LinkList />;
+    return <CreateLink/>;
+  };
+}
+
+export default App;
